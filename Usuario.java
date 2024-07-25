629a6856f74be8b56cedc4c58d73363ee66e08da
Giselle Porto <giselledesouzaportosilva@gmail.com>
2024-07-25 11:22:19 -0300

    class Usuario

---
+++ Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario{
+    private String emailInstitucional;
+    private String senha;
+
+    public Usuario(String emailInstitucional, String senha){
+        setEmailInstitucional(emailInstitucional);
+        setSenha(senha);
+    }
+
+    public String getEmailInstitucional(){
+        return this.emailInstitucional;
+    }
+
+    public void setEmailInstitucional(String emailInstitucional){
+        this.emailInstitucional = emailInstitucional;
+    }
+
+    public String getSenha(){
+        return this.senha;
+    }
+
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+}
