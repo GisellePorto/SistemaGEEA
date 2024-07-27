0aadfda8249f52cb8d606e46e54d94b9b5fc9574
Giselle Porto <giselledesouzaportosilva@gmail.com>
2024-07-27 11:46:56 -0300

    class Espaco

---
+++ Espaco.java
@@ -0,0 +1,15 @@
+public class Espaco {
+    private String nome;
+
+    public Espaco(String nome){
+        setNome(nome);
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+}
