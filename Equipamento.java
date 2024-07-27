bcb4b001086066a950e73e2f8e5a65e5f44e1f57
Giselle Porto <giselledesouzaportosilva@gmail.com>
2024-07-27 11:18:06 -0300

    class Equipamento

---
+++ Equipamento.java
@@ -0,0 +1,15 @@
+public class Equipamento {
+    private String nome;
+
+    public Equipamento(String nome){
+        setNome(nome);
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+}
