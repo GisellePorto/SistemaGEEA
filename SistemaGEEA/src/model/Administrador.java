5cfa05c4eb23d1f264a5d82b081d8552aa36ed19
K4aiodu <kaioeduardo844@gmail.com>
2024-09-01 17:17:47 -0300

    Ultima alt

--- SistemaGEEA/src/model/Administrador.java
+++ SistemaGEEA/src/model/Administrador.java
@@ -6,11 +6,7 @@ public class Administrador extends Usuario{
 
         super(emailInstitucional, senha);
     }
-    //metodo aprovarReserva, nele o administrador deve ser capaz de autorizar o pedido de reserva recebido
-    public void aprovarReserva(Reserva reserva) {
-        reserva.setAprovacao(true);
-        System.out.println("Reserva aprovada: " + reserva);
-    }
+
     //metodo rejeitarReserva, nele o administrador deve ser capaz de rejeitar o pedido de reserva recebido
 
     public void rejeitarReserva(Reserva reserva) {
@@ -18,16 +14,5 @@ public class Administrador extends Usuario{
         System.out.println("Reserva rejeitada: " + reserva);
     }
 
-    public void cadastrarEspaco(String nome, int numSerie){
-        //metodo para cadastrar um espaco e armazenar no estoque
-    }
 
-    public void manutencao(Usuario usuario, Equipamento equip, String descricao) {
-        //metodo manutencao, que recebe a solicitacao de manutencao e é notificado, para agir sobre a situacao
-    }
-
-    public void manutencao(Usuario usuario, Espaco espaco, String descricao) {
-        //metodo manutencao, que recebe a solicitacao de manutencao e é notificado, para agir sobre a situacao
-        //talvez em versoes futuras procu
-    }
 }
