208fac3a37aced9ed8ae23755c47713d38a29338
K4aiodu <kaioeduardo844@gmail.com>
2024-09-01 17:26:06 -0300

    Ultima alt

--- SistemaGEEA/src/model/Professor.java
+++ SistemaGEEA/src/model/Professor.java
@@ -34,7 +34,8 @@ public class Professor extends Usuario{
         //metodo solicitarEspaco que permite que o usuario do tipo professor solicitar uma reserva de espaco a ser aprovada
     }
 
-
+    //metodo visualizar: os professores so podem ver as salas, mas nao podem solicitar reserva. independente dessa informacao, os alunos devem ser capazes
+    // de visualizar todo o estoque de artefatos do sistema//
     public void visualizarEstoque(Estoque estoque) {
         System.out.println("Espaços disponíveis:");
         for (Espaco espaco : estoque.getEspacos()) {
@@ -46,10 +47,24 @@ public class Professor extends Usuario{
             System.out.println("- " + equipamento.getNome());
         }
 
-        //metodo visualizarEstoque, onde os professores devem ser capazes de visualizar
+
     }
+
+    /**metodo solicitarManutencao, que deve permitir que o usuario solicite a manutencao de um determinado espaco, geralmente relacionado a limpeza; este metodo deve se
+     comunicar com a classe Administrador**/
     public void solicitarManutencao (Usuario usuario,String comentario,Equipamento equipamento, LocalTime horario,LocalDate data){
-        SolicitarManutencao manutencao = new SolicitarManutencao(usuario,comentario,equipamento,horario,data);
+        if (usuario == null || equipamento == null || comentario == null || comentario.isEmpty()) {
+            throw new IllegalArgumentException("Dados insuficientes para solicitar a manutenção.");
+        }
+        if (horario == null || data == null) {
+            throw new IllegalArgumentException("Data e horário devem ser fornecidos.");
+        }
+
+        SolicitarManutencao manutencao = new SolicitarManutencao(usuario, comentario, equipamento, horario, data);
+        GerenciaManutencao gerenciaManutencao = new GerenciaManutencao();
+        gerenciaManutencao.addManutencaoPendente(manutencao);
+
+
 
         /**metodo solicitarManutencao, que deve permitir que o usuario solicite a manutencao de um determinado espaco, geralmente relacionado a limpeza; este metodo deve se
          comunicar com a classe Administrador**/
