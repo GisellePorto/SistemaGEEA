c752d96e6503351f5621ce694909c09c5fa9542a
K4aiodu <kaioeduardo844@gmail.com>
2024-09-01 17:23:07 -0300

    Ultima alt

--- SistemaGEEA/src/model/GerenciaManutencao.java
+++ SistemaGEEA/src/model/GerenciaManutencao.java
@@ -1,4 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciaManutencao {
+    private List<SolicitarManutencao> manutencoesPendentes = new ArrayList<>();
+    private List<SolicitarManutencao> manutencoesConcluidas = new ArrayList<>();
+
+
+    public  void addManutencaoPendente(SolicitarManutencao manutencao) {
+        manutencoesPendentes.add(manutencao);
+    }
+
+    public void concluirManutencao(SolicitarManutencao manutencao) {
+        if (manutencoesPendentes.remove(manutencao)) {
+            manutencoesConcluidas.add(manutencao);
+        } else {
+            System.out.println("Manutenção não encontrada na lista de pendentes.");
+        }
+    }
+
+    public List<SolicitarManutencao> getManutencoesPendentes() {
+        return manutencoesPendentes;
+    }
+
+    public List<SolicitarManutencao> getManutencoesConcluidas() {
+        return manutencoesConcluidas;
+    }
+
+
 }
