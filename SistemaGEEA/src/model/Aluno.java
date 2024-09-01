f3dfbd7d03d7509b60ce86f85b3620c6a4939e72
K4aiodu <kaioeduardo844@gmail.com>
2024-09-01 17:19:19 -0300

    Ultima alt

--- SistemaGEEA/src/model/Aluno.java
+++ SistemaGEEA/src/model/Aluno.java
@@ -3,10 +3,10 @@ package model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Aluno extends Usuario{
+public class Aluno extends Usuario {
     private int matricula;
 
-    public Aluno(String emailInstitucional, String senha, int matricula){
+    public Aluno(String emailInstitucional, String senha, int matricula) {
         super(emailInstitucional, senha);
         this.matricula = matricula;
     }
@@ -15,36 +15,54 @@ public class Aluno extends Usuario{
         return matricula;
     }
 
-    public void setMatricula(int matricula){
+    public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
+
     //metodo solicitar:
-    public void solicitarEquipamento(String nome,Equipamento equipamento, LocalTime horario, LocalDate data, String descricao){
-        Equipamento equipament= new Equipamento(nome,equipamento,horario,data,descricao);
+    public void solicitarEquipamento(String nome, Equipamento equipamento, LocalTime horario, LocalDate data, String descricao) {
+        Equipamento equipament = new Equipamento(nome, equipamento, horario, data, descricao);
 
 //esboco da classe de solicitacao de equipamentos, onde o aluno deve ser capaz de solicitar uma reserva a ser aprovada ou nao pelo administrador
     }
 
-    public  void solicitarReserva(Usuario usuario,Espaco espaco, LocalTime horario, LocalDate data, String descricao) {
+    public void solicitarReserva(Usuario usuario, Espaco espaco, LocalTime horario, LocalDate data, String descricao) {
         Reserva reserva = new Reserva(usuario, espaco, horario, data, descricao);
+
 
 
         //metodo visualizar: os alunos so podem ver as salas, mas nao podem solicitar reserva. independente dessa informacao, os alunos devem ser capazes
         // de visualizar todo o estoque de artefatos do sistema//
+        public void visualizaEstoque (Estoque estoque) {
+            System.out.println("Espaços disponíveis:");
+            for (Espaco espaco : estoque.getEspacos()) {
+                System.out.println("- " + espaco.getNome());
+            }
 
-        public void visualizarEstoque( ) {
+            System.out.println("\nEquipamentos disponíveis:");
+            for (Equipamento equipamento : estoque.getEquipamentos()) {
+                System.out.println("- " + equipamento.getNome());
+            }
 
-            //metodo visualizarEstoque, onde o aluno deve ser capaz de visualizar os espacos e equipamentos disponiveis no sistema
+            //metodo visualizarEstoque, onde os professores devem ser capazes de visualizar
         }
 
-        public void solicitarManutencao (Usuario usuario,String comentario,Equipamento equipamento, LocalTime horario,LocalDate data){
-            SolicitarManutencao manutencao = new SolicitarManutencao(usuario,comentario,equipamento,horario,data);
+        /**metodo solicitarManutencao, que deve permitir que o usuario solicite a manutencao de um determinado espaco, geralmente relacionado a limpeza; este metodo deve se
+         comunicar com a classe Administrador**/
+        public void solicitarManutencao (Usuario usuario, String comentario, Equipamento equipamento, LocalTime
+        horario, LocalDate data){
+            if (usuario == null || equipamento == null || comentario == null || comentario.isEmpty()) {
+                throw new IllegalArgumentException("Dados insuficientes para solicitar a manutenção.");
+            }
+            if (horario == null || data == null) {
+                throw new IllegalArgumentException("Data e horário devem ser fornecidos.");
+            }
 
-
+            SolicitarManutencao manutencao = new SolicitarManutencao(usuario, comentario, equipamento, horario, data);
+            GerenciaManutencao gerenciaManutencao = new GerenciaManutencao();
+            gerenciaManutencao.addManutencaoPendente(manutencao);
 
         }
-        //metodo solicitarManutencao, que deve permitir que o usuario solicite a manutencao de um determinado equipamento; este metodo deve se
-        //comunicar com a classe Administrador**//
-
 
     }
+}
